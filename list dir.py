b4e8b0c3a995be1b4d6559cf00de53ffd4044d11
sparkz4 <quin5_in@yahoo.com>
2021-12-21 16:40:48 +0530

    add comment

--- list dir.py
+++ list dir.py
@@ -2,5 +2,25 @@ import os
 from playsound import playsound
 print("Hello")
 print(os.listdir())
-playsound('C:\\Users\\spark\\Downloads\\chillabstract12099.mp3')
-# list dir with sound
+# list dir with sound 
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        #num1 = float(input("Enter first number: "))
+        # num2 = float(input("Enter first number: "))
+        if choice == '1':
+            playsound('C:\\Users\\spark\\Downloads\\chillabstract12099.mp3')
+
+        elif choice == '2':
+            playsound('C:\\Users\\spark\\Downloads\\techno.mp3')
+
+# check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+
+else:
+        print("Invalid Input")
